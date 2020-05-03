7372ec81fb8afcd064bfb7b9bafc032cc3daf35a
Bo-Yi Wu <appleboy.tw@gmail.com>
2020-05-03 21:37:10 +0800

    chore: update readme

--- example/main.go
+++ example/main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"fmt"
-	"regexp"
+	"net/http"
 	"time"
 
 	"github.com/gin-contrib/requestid"
 	"github.com/gin-gonic/gin"
-)
-
-var (
-	rxURL = regexp.MustCompile(`^/regexp\d*`)
 )
 
 func main() {
@@ -21,7 +17,7 @@ func main() {
 
 	// Example ping request.
 	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong "+fmt.Sprint(time.Now().Unix()))
+		c.String(http.StatusOK, "pong "+fmt.Sprint(time.Now().Unix()))
 	})
 
 	// Listen and Server in 0.0.0.0:8080
